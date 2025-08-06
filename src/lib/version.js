4b1beb077757bceab8873a83f3aa61ec4a1d256a
Matt Olson <ucf.molson@gmail.com>
2025-08-05 20:26:50 -0400

    version

--- src/lib/version.js
+++ src/lib/version.js
@@ -1,5 +1,5 @@
 export const version = {
   number: '1.1.0',
-  buildDate: '2025-07-14', // Update this when you deploy
-  display: 'v1.1.0 (2025-07-16)'
+  buildDate: '2025-08-05', // Update this when you deploy
+  display: 'v1.1.0 (2025-08-05)'
 };
