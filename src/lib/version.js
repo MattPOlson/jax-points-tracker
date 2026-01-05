0bc7c18ef27091c70388436873f484fedc8bc031
Matt Olson <ucf.molson@gmail.com>
2026-01-05 16:45:53 -0500

    Update version number and build date

--- src/lib/version.js
+++ src/lib/version.js
@@ -1,5 +1,5 @@
 export const version = {
-  number: '1.3.3',
-  buildDate: '2025-09-30', // Update this when you deploy
-  display: 'v1.3.4 (2025-12-18)'
+  number: '1.3.5',
+  buildDate: '2026-01-05', // Update this when you deploy
+  display: 'v1.3.5 (2026-01-05)'
 };
