1422bc9d211d4d02d3f246b461e08a30a44480fd
Matt Olson <ucf.molson@gmail.com>
2026-04-09 15:43:04 -0400

    Update version to 2.0.0 with new build date

--- src/lib/version.js
+++ src/lib/version.js
@@ -1,5 +1,5 @@
 export const version = {
-  number: '1.4.0',
-  buildDate: '2026-03-25', // Update this when you deploy
-  display: 'v1.4.0 (2026-03-25)'
+  number: '2.0.0',
+  buildDate: '2026-04-09', // Update this when you deploy
+  display: 'v2.0.0 (2026-04-09)'
 };
